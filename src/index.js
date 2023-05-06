e539b0d3e3c39051333a14a4a893ffb1d4658b1e
Peter <petersolomon704@gmail.com>
2023-05-06 09:16:49 +0100

    Part 7-2
    Using redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import { Provider } from "react";
 import App from "./App";
+import { store } from "./redux/store";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    {" "}
+    <App />{" "}
+  </Provider>
+);
