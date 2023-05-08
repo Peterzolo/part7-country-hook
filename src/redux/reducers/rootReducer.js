6e941f3196a07488b35441f67c8c0a2b20688ad8
Peter <petersolomon704@gmail.com>
2023-05-08 10:14:04 +0100

    Update rootReducer.js
    Added user reducer to root reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import notificationReducer from "./notification/notificationReducer";
 import blogReducer from "./blog/blogReducer";
+import userReducer from "./user/userReducer";
 
 export const rootReducer = combineReducers({
   blogs: blogReducer,
   notification: notificationReducer,
+  users: userReducer,
 });
