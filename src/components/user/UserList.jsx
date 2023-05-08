e508578de4f93c690fad64cf40b440b4cb1e8c6c
Peter <petersolomon704@gmail.com>
2023-05-08 15:20:47 +0100

    Update UserList.jsx
    Modified code

--- src/components/user/UserList.jsx
+++ src/components/user/UserList.jsx
@@ -4,20 +4,21 @@ import { getAllUserFromService } from "../../redux/actions/userAction";
 
 const UserList = () => {
   const dispatch = useDispatch();
-  const users = useSelector((state) => state.users);
+  const { users } = useSelector((state) => state.users);
+  console.log("SELECTOR USERS", users);
   useEffect(() => {
     dispatch(getAllUserFromService());
   }, [dispatch]);
   return (
     <div>
       <h1>User List</h1>
-      {users &&
+      {/* {users &&
         users.map((user) => (
           <div key={user.id}>
             <h2>{user.name}</h2>
             <p>{user.email}</p>
           </div>
-        ))}
+        ))} */}
     </div>
   );
 };
